9ff0b306eaf330ced09a60c41584e42059db4345
Kevinoh <149471098+Kevin-Aluth@users.noreply.github.com>
2025-12-04 00:03:48 +0100

    better frontend

--- app/routes/auth_bp.py
+++ app/routes/auth_bp.py
@@ -22,6 +22,13 @@ def login():
 
     return render_template('authentication/login.html', form=form)
 
+@auth_bp.route('/logout', methods=['GET'])
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+    
+    return redirect(url_for('auth.login'))
+
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register(): 
     form = RegisterForm()
